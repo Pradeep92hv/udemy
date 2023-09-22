e0a9a7a5094b52dbb4da2a5a4758fb58b825d7cf
Pradeep92hv <hvpradeep92@gmail.com>
2023-09-22 21:22:02 +0530

    user athentication

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'food.apps.FoodConfig'
+     'food.apps.FoodConfig',
+     'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL='food:index'
+LOGIN_URL='login'
+
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    if request.method =='POST':
+        form=RegisterForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            print(username)
+            messages.success(request,f'welcome {username},your account is created')
+            return redirect('food:login')
+    else:
+        form=RegisterForm()
+        print('hi')
+    return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
